4cc6acfa4148a899fd6c67153e6b6618fecfabe7
Aleksey Palanevich <palanevich.aleksey@gmail.com>
2020-10-01 13:32:54 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
+exports.min = function min(array) {
+    if (!array) return 0;
 
-exports.min = function min (array) {
-  return 0;
-}
+    let minNum = array[0];
+    array.forEach((num) => {
+        minNum = minNum < num ? minNum : num;
+    });
 
-exports.max = function max (array) {
-  return 0;
-}
+    return minNum || 0;
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!array) return 0;
+
+    let maxNum = array[0];
+    array.forEach((num) => {
+        maxNum = maxNum > num ? maxNum : num;
+    });
+    return maxNum || 0;
+};
+
+exports.avg = function avg(array) {
+    if (!array) return 0;
+
+    return array.reduce((sum, num) => sum + num, 0) / array.length || 0;
+};
